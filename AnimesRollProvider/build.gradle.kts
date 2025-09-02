d24822d9939e6711d880f1a40db5681ab519d15e
Savio Oliveira <54548469+savio-dev@users.noreply.github.com>
2025-09-02 15:05:32 -0300

    Update build.gradle.kts

--- AnimesRollProvider/build.gradle.kts
+++ AnimesRollProvider/build.gradle.kts
@@ -7,13 +7,3 @@ cloudstream {
     tvTypes = listOf("Anime")
     iconUrl = "https://www.anroll.net/favicon.ico"
 }
-
-// Task para gerar o JAR do plugin
-tasks.register<Jar>("buildPluginJar") {
-    archiveBaseName.set("AnimesROLLPlugin")
-    archiveVersion.set("1.0")
-    from(project.the<SourceSetContainer>()["main"].output)
-    manifest {
-        attributes["Main-Class"] = "com.example.animesroll.AnimesRollProvider"
-    }
-}
